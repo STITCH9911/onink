9715fbf7328bac056fc5987153ba9f5640ecf39c
STITCH <joseitohern99@gmail.com>
2023-06-24 18:55:03 -0400

    add metodos para gestionar archivos

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QDate
-
+import os
 
 
 def get_birthday(ci: str) -> QDate:
@@ -18,3 +18,15 @@ def get_age(ci):
     fecha_nacimiento = get_birthday(ci=ci)
     hoy = QDate.currentDate()
     return fecha_nacimiento.daysTo(hoy) // 365
+
+def file_exists(name: str, folder: str = 'clients_pictures') -> str|bool:
+    for file_name in os.listdir(folder):
+        file_base, file_ext = os.path.splitext(file_name)
+        if file_base == name:
+            return file_name
+    return False
+
+def delete_file(name: str, folder: str = 'clients_pictures'):
+    if file_exists(name,folder):
+        file_path = os.path.join(folder,file_exists(name,folder))
+        os.remove(file_path)
