5405c6e8853c1ec94f9b9612415392ca014c67b3
STITCH <joseitohern99@gmail.com>
2023-08-25 03:49:01 -0400

    Update ClientWorks Widget

--- ClientWorks.py
+++ ClientWorks.py
@@ -1,24 +1,33 @@
 import os
 from typing import List, Tuple
+from PyQt6 import QtGui
 from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QShowEvent
+
 from payment import Payment
 from views.clientWorks_ui import Ui_ClientWorks
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QStackedWidget
 from config import DEFAULT_PICTURE, Session
-
 from models import Clients, Trabajos
 from strippedTable import StripedTable
 from utils import BACK_ARROW, CHANGE_PAGE, default_image, file_exists
 
 
 class ClientWorks(QWidget, Ui_ClientWorks):
-    def __init__(self, parent: QWidget = None) -> None:
+    def __init__(self, client = None, parent: QWidget = None) -> None:
         super().__init__(parent)
         self.setupUi(self)
         self.table = None
+        self.client = client
         self.mainWindowWidget  = self.parentWidget().parentWidget().parentWidget().parentWidget().parentWidget().parentWidget()
         self.bt_back.setIcon(BACK_ARROW)
         self.bt_change_page.setIcon(CHANGE_PAGE)
+        self.bt_back.clicked.connect(self.back)
+        self.bt_change_page.clicked.connect(self.change_page)
+
+    def showEvent(self, a0: QShowEvent) -> None:
+        self.setClient()
+        return super().showEvent(a0)
 
     def setPic(self):
         ci = file_exists(self.client.ci, 'clients_pictures')
@@ -26,10 +35,9 @@ class ClientWorks(QWidget, Ui_ClientWorks):
             ci = DEFAULT_PICTURE
         default_image(self.lb_pic_works_client, ci, 'clients_pictures')
 
-
-    def setClient(self, client: Clients):
+    def setClient(self):
         with Session() as session:
-            self.client = session.merge(client)
+            self.client = session.merge(self.client)
             self.setPic()
             self.le_ci_client_works.setText(self.client.ci)
             self.le_full_name_client_works.setText(self.client.nombre_apellidos)
@@ -61,10 +69,26 @@ class ClientWorks(QWidget, Ui_ClientWorks):
                 data.append([w.tipo_trabajo.tipo, str(w.price), tipo_pago, w.created_at.strftime('%d-%m-%Y')])
         return headers, data, dropdown_buttons, trabajos
     
-    def setFunciones(self, funcVolver, funcChangePage):
-        self.bt_back.clicked.connect(funcVolver)
-        self.bt_change_page.clicked.connect(funcChangePage)
-    
     def payWork(self, obj: Trabajos):
         payment = Payment(obj, self, self.mainWindowWidget)
         payment.exec()
+    
+    def back(self):
+        from ClientsList import ClientListWidget
+        stacked: QStackedWidget = self.parentWidget()
+        w = stacked.findChildren(ClientListWidget)
+        for i in w:
+            i.setParent(None)
+        w = ClientListWidget(parent=stacked)
+        stacked.addWidget(w)
+        stacked.setCurrentWidget(w)
+
+    def change_page(self):
+        from showClient import ShowCLient
+        stacked: QStackedWidget = self.parentWidget()
+        w = stacked.findChildren(ShowCLient)
+        for i in w:
+            i.setParent(None)
+        w = ShowCLient(parent=stacked, client=self.client)
+        stacked.addWidget(w)
+        stacked.setCurrentWidget(w)
